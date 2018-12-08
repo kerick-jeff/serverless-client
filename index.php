bef0213bf492c05f889457b5d8f2c580c61838f4
k3r1ck <frukerickjeff@gmail.com>
2018-12-08 14:55:59 +0100

    Implemented recaptcha on client-side

--- index.php
+++ index.php
@@ -15,19 +15,42 @@
 
             body {
                 display: flex;
+                flex-flow: column;
                 align-items: center;
                 justify-content: center;
             }
 
+            .info {
+                padding: 25px;
+                border-radius: 4px;
+                color: #ffffff;
+                font-size: 1em;
+                margin-bottom: 5vh;
+                text-align: center;
+            }
+
+            .info.info-success {
+                background-color: #28a745;
+                border: 1px solid #28a745;
+            }
+
+            .info.info-error {
+                background-color: #dc3545;
+                border: 1px solid #dc3545;
+            }
+
+            h3 {
+                color: #007bff;
+            }
+
             form {
-                width: 80%;
                 padding: 25px;
                 border: 1px solid #cccccc;
                 background-color: #f4f4f4;
                 border-radius: 4px;
             }
 
-            form input {
+            form input, form button {
                 font-size: 1em;
                 padding: 8px 16px;
                 border-radius: 4px;
@@ -38,17 +61,21 @@
                 border: 1px solid #f4f4f4;
             }
 
-            form input[type="submit"] {
+            form button {
                 color: #ffffff;
+                margin-top: 2vh;
                 background-color: #007bff;
                 border: 1px solid #007bff;
             }
 
-            form input[type="submit"]:hover {
+            form button:hover {
                 background-color: #f4f4f4;
                 color: #555555;
             }
         </style>
+
+        <!-- Include Google Recaptcha Javscript API -->
+        <script src='https://www.google.com/recaptcha/api.js' async defer></script>
 
         <!-- A little JavaScript to handle clientside interactions -->
         <script type="text/javascript">
@@ -58,16 +85,42 @@
                 const userAgent = window.navigator.userAgent;
 
                 // Set the dataForm action attribute
-                document.dataForm.action = 'https://us-central1-awesome-96.cloudfunctions.net/processFormData?userAgent=' + userAgent;
+                document.dataForm.action = 'https://us-central1-awesome-96.cloudfunctions.net/processFormData?userAgent=' + userAgent + '&redirectTo=' + window.location.href;
+                
+                document.getElementById("submit-btn").onclick = validate;
             });
+
+            function onSubmit(token) {
+                // Perform server-side verification
+                console.log(token)
+
+                // Handle server-side recaptcha verification. Then, submit form
+                document.dataForm.submit();
+            }
+
+            function validate(event) {
+                if (document.dataForm.checkValidity()) {
+                    grecaptcha.execute();
+                    event.preventDefault();
+                }
+            }
         </script>
     </head>
     <body>
+        <?php if (isset($_GET['success']) && $_GET['success'] == 'true' && isset($_GET['msg'])) { ?>
+            <div class="info info-success"><?php echo $_GET['msg'] ?></div>
+        <?php } elseif(isset($_GET['success']) &&  $_GET['success'] == 'false' && isset($_GET['msg'])) { ?>
+            <div class="info info-error"><?php echo $_GET['msg'] ?></div>
+        <?php } ?>
+
+        <h3>Please fill all fields appropriately</h3>
+        
         <form name="dataForm" method="POST">
-            <input name="firstName" type="text" placeholder="First Name">
-            <input name="lastName" type="text" placeholder="Last Name">
-            <input name="email" type="email" placeholder="Email Address">
-            <input type="submit" value="Submit">
+            <input name="firstName" type="text"  placeholder="First Name" required>
+            <input name="lastName" type="text" placeholder="Last Name" required>
+            <input id="email" name="email" type="email" placeholder="Email Address" required>
+            <div id='recaptcha' class="g-recaptcha" data-sitekey="6LcyUn8UAAAAAIdDyoVosxKYD007fdNfufJqkUlu" data-callback="onSubmit" data-size="invisible"></div>
+            <button id="submit-btn">Submit</button>
         </form>
     </body>
-</html> 
+</html>
