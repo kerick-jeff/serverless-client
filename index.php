4b8fe2ab9285674868e4bca44e0cdf9bbc28dc03
k3r1ck <frukerickjeff@gmail.com>
2018-12-09 14:46:56 +0100

    Updated index.php file

--- index.php
+++ index.php
@@ -129,7 +129,7 @@
                 <?php echo $_GET['msg'] ?>
                 <?php if ($_GET['publicUrl']) { ?>
                     <div>
-                        <a href="<?php echo $_GET['publicUrl'] ?>" _target="blank">View File</a>
+                        <a href="<?php echo $_GET['publicUrl'] ?>" target="_blank">View File</a>
                     </div>
                 <?php } ?>
             </div>
